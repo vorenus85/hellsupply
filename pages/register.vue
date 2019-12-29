3283c045a7b7b35634e84c03c4696643da1ed6ee
Perge Janos <pergejanos@webshopexperts.hu>
2019-12-29 12:20:11 +0100

    create register layout

--- pages/register.vue
+++ pages/register.vue
@@ -1,14 +1,33 @@
 <template lang="pug">
-  div ITS DA REG
-    br
-    v-btn(outlined :to="{ name: 'login' }" nuxt) Login
+  div Create account
+    v-form(ref="form" @submit="tryLogin" v-model="valid")
+      v-text-field(v-model="lastname" label="Lastname" :rules="[v => !!v || 'Lastname is required']" required type="text")
+      v-text-field(v-model="firstname" label="Firstname" :rules="[v => !!v || 'Firstname is required']" required type="text")
+      v-text-field(v-model="email" label="Email" :rules="emailRules" required type="email")
+      v-text-field(v-model="password" label="Password" :rules="[v => !!v || 'Username is required']" type="password" required)
+      v-btn(tile color="success" type="submit") register
+      v-spacer
+      br
+      br
+      v-btn(outlined :to="{ name: 'login' }" nuxt) Login
 </template>
 
 <script>
-export default {
-  layout: 'centered',
-  head: () => ({
-    title: 'Register'
-  })
-}
+  export default {
+    layout: 'centered',
+    head: () => ({
+      title: 'Register'
+    }),
+    data: () => ({
+      valid: false,
+      lastname: '',
+      firstname: '',
+      email: '',
+      password: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ]
+    })
+  }
 </script>
