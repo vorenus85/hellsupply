f9056384d0884a28b5840c217d483aa50de252ca
Perge Janos <pergejanos@webshopexperts.hu>
2019-12-29 12:17:36 +0100

    add login.js for admin,admin login

---
+++ login.js
@@ -0,0 +1,43 @@
+require('dotenv-flow').config()
+const { MongoClient } = require('mongodb')
+const { pino } = require('./server/logger')
+const MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_DB = process.env.MONGODB_DB
+if (!MONGODB_DB) {
+  let uriSplit = MONGODB_URI.split('/')
+  uriSplit = uriSplit[uriSplit.length - 1].split('?')
+  MONGODB_DB = uriSplit[0]
+}
+pino.info('Create DB connection', { MONGODB_URI, MONGODB_DB })
+MongoClient.connect(
+  MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, client) => {
+    if (err) {
+      pino.error(err, `MongoDB not connected`)
+      return
+    }
+    pino.info(`MongoDB connected`)
+    const db = client.db(MONGODB_DB)
+    const users = db.collection('users')
+    pino.info('Inserting example user (admin:admin)')
+    try {
+      users.insertOne({
+        firstName: "It's A",
+        lastName: 'Admin',
+        username: 'admin',
+        password:
+          '$2b$15$gHNKCQPydZ/R.ZH/nqOmm.zGalFn/UuYEV4tUkfsGHBpIgQx9tUyG',
+        role: 'admin',
+        active: true
+      })
+      pino.info('Example user (admin:admin) inserted successfully')
+    } catch (e) {
+      pino.error(e, 'Cannot insert example user')
+    }
+    client.close()
+  }
+)
